12bf51d9d6b34e64536aecd9b7842eb2135010b7
Sergey Khmelevskoy <xs290@me.com>
2018-05-28 02:23:48 +0300

    add files

---
+++ app/models/signup_lead.rb
@@ -0,0 +1,2 @@
+class SignupLead < ApplicationRecord
+end
---
+++ db/migrate/20180527232334_create_signup_leads.rb
@@ -0,0 +1,19 @@
+class CreateSignupLeads < ActiveRecord::Migration[5.1]
+  def change
+    create_table :signup_leads do |t|
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :company_name
+      t.string :phone
+      t.string :company_industry
+      t.string :company_old
+      t.string :company_employees
+      t.date :meeting_date
+      t.string :meeting_time
+      t.boolean :ispending
+
+      t.timestamps
+    end
+  end
+end
