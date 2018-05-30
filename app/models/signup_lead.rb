32db184d8cfe8bc6b493071869a9675324d0f166
Sergey Khmelevskoy <xs290@me.com>
2018-05-31 00:52:24 +0300

    don't validate uniquness of email

--- app/models/signup_lead.rb
+++ app/models/signup_lead.rb
@@ -1,7 +1,7 @@
 class SignupLead < ApplicationRecord
   validates :email, presence: { message: "Please enter your email" }
   validates_email_format_of :email, :message => 'Email format is not correct'
-  validates_uniqueness_of :email, :message => 'We have already scheduled an appointment'
+  # validates_uniqueness_of :email, :message => 'We have already scheduled an appointment'
 
   after_save :place_to_zapier
 
