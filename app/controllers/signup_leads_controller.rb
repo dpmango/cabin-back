3908348b58b96cccd16a2fc9839b370224d598e9
Sergey Khmelevskoy <xs290@me.com>
2018-07-11 23:51:13 +0300

    add isfollowup to the whitelist

--- app/controllers/signup_leads_controller.rb
+++ app/controllers/signup_leads_controller.rb
@@ -35,6 +35,6 @@ class SignupLeadsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def signup_lead_params
-      params.require(:signup_lead).permit(:email, :first_name, :last_name, :company_name, :phone, :company_industry, :company_old, :company_employees, :meeting_date, :meeting_time, :ispending, :pricing_plan, :pricing_options, :email_instead)
+      params.require(:signup_lead).permit(:email, :first_name, :last_name, :company_name, :phone, :company_industry, :company_old, :company_employees, :meeting_date, :meeting_time, :ispending, :isfollowup, :pricing_plan, :pricing_options, :email_instead)
     end
 end
