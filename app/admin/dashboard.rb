e7f2854faead74c2f727646a51c21274fb5e8136
Sergey Khmelevskoy <xs290@me.com>
2018-10-23 17:24:46 +0300

    modify admin to prevent blank records error

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,12 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -17,7 +17,7 @@ ActiveAdmin.register_page "Dashboard" do
         panel "Recent Leads" do
           ul do
             SignupLead.last(10).map do |lead|
-              li lead.first_name + " " + lead.last_name + " - " + lead.email + " (" + lead.created_at.strftime("%m/%d/%Y at %I:%M%p") + ")"
+              li (lead.first_name ? lead.first_name : "") + " " + (lead.last_name ? lead.last_name : "") + " - " + (lead.email ? lead.email : "") + " (" + lead.created_at.strftime("%m/%d/%Y at %I:%M%p") + ")"
             end
           end
         end
